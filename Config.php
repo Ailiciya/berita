5a31adc0c95fb7a8bb0fcee0f4d864ee3d273758
Elysia <114798157+Ailiciya@users.noreply.github.com>
2024-06-19 22:04:05 +0700

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,5 +1,5 @@
 <?php
-$config = mysqli_connect("localhost","root", "","berita_4556");
+$config = mysqli_connect("localhost","root", "","berita_4870");
 if (!$config) {
     die('Gagal terhubung ke MySQLi :'.mysqli_connect_error());
 }
